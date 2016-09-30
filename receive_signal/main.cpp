14d665e3c5b95c2c1d8edd23dc9b019ace3576ac
RikHU <rik.ruttenberg@student.hu.nl>
2016-09-30 12:23:19 +0200

    betere versie receive_signal

--- receive_signal/main.cpp
+++ receive_signal/main.cpp
@@ -1,38 +1,24 @@
 #include "hwlib.hpp"
-/*
-int main( void ){	
-   // kill the watchdog
-   WDT->WDT_MR = WDT_MR_WDDIS;
-   
-   namespace target = hwlib::target;
-   
-   auto led0 = target::pin_out( target::pins::d7 );
-   auto led1 = target::pin_out( target::pins::d6 );
-   auto led2 = target::pin_out( target::pins::d5 );
-   auto led3 = target::pin_out( target::pins::d4 );
-   
-   auto leds = hwlib::port_out_from_pins( led0, led1, led2, led3 );
-   hwlib::kitt( leds );
-}
- */
-bool receiver(hwlib::pin_in & receive_pin){
-    hwlib::cout << "plek 1";
-    while(receive_pin.get() == 1);
-    hwlib::cout << "plek 2";
-    auto time1 = hwlib::now_us();
-    hwlib::cout << "plek 3";
-    while(receive_pin.get() == 0);
-    hwlib::cout << "plek 4";
-    
-    auto time2 = hwlib::now_us();
-    auto result_time = time2-time1;
 
-    if(result_time == 1600){
-        return true;
-    }
-    else{
-        return false;
-    }
+void receiver(hwlib::pin_in & receive_pin){
+   // hwlib::cout << "plek 1";
+   if(receive_pin.get() == 1){
+       for(int i = 0; i < 8; i++){
+               while(receive_pin.get() == 1);
+           // hwlib::cout << "plek 2";
+                auto time1 = hwlib::now_us();
+           //hwlib::cout << "plek 3";
+                while(receive_pin.get() == 0);
+           // hwlib::cout << "plek 4";
+                auto time2 = hwlib::now_us();
+                auto result_time = time2-time1;
+               
+                if result_time >= 4000000 break;
+                if(result_time >= 800 && result_time <= 900) hwlib::cout << '1';
+                if(result_time >= 1600 && result_time <= 1700) hwlib::cout << '0';
+        }
+        hwlib::cout << '\n';
+   }
 }
 
 int main(void){
@@ -49,8 +35,7 @@ int main(void){
 
     while(1){
         //hwlib::cout << "check";
-        hwlib::cout << receiver(r_pin);
-        hwlib::cout << '\n';
+        receiver(r_pin);
         //hwlib::wait_ms(1);
     }
     return 0;
