3996098b762c9ad5e3829fd4cb2a378c6c3a0a8b
timijntema <timijntema@192.168.2.1>
2016-10-13 20:50:44 +0200

    Doxygen and casus file added
    This upload contains an version of the used casus for this project and
    the doxygen comments for the zero version of the IR_message protocol.

--- 0-Versie-Protocol/receive_ir_message.hpp
+++ 0-Versie-Protocol/receive_ir_message.hpp
@@ -3,18 +3,55 @@
 #ifndef RECEIVE_IR_MESSAGE_HPP
 #define RECEIVE_IR_MESSAGE_HPP
 
+///Class for receiving IR message
+//
+///This class has been created to easily receive an IR message. It uses the protocol specified
+/// in the document "V2THDE - Casus lasertag 2016-2017.pdf". The class uses hwlib created
+// by Wouter van Ooijen to make programming in c++ for Arduino and other microcontrollers
+/// more easy.
 class receive_ir_message
 {
 private:
-        hwlib::target::pin_in &receiver_pin;
-        int received_id;
-        int received_weapon;
+        hwlib::target::pin_in & receiver_pin;///IR receiver pin
+        int received_id;///received player identifier
+        int received_weapon;///received weapon identifier
 public:
-    receive_ir_message(hwlib::target::pin_in &receiver_pin);
+    ///Default constructor
+    //
+    ///The default constructor needs a pin to wich the IR reciever has been attached.
+    receive_ir_message(hwlib::target::pin_in & receiver_pin);
+    
+    ///Default destructor
+    //
+    ///The destructor releases the memory used by the class to be used for other objects.
     ~receive_ir_message();
+    
+    ///Get one bit from the bitstream
+    //
+    ///This function gets one bit from the incoming bitstream and returns a boolean to
+    /// confirm if it is a one or a zero. It does not need any parameters. The bit is
+    /// received according to the above mentioned document.
     bool get_bit();
+    
+    ///Get bits from IR receiver
+    //
+    ///This functions get fifteen bits from the IR receiver and puts them in an integer.
+    ///This integer is then returned so it can be decoded by the decoder. The function
+    /// dus not need any parameters.
     int get_byte();
+    
+    ///Decode the bitstream
+    //
+    ///This function decodes the bitstream in the way specified in the above mentioned
+    /// document. It also checks the bitstream with an exor to make sure it is authentic.
+    /// The function needs the bitstream as a parameter and returns zero or one depending
+    /// on the result of the exor.
     int decoder(int receiver_byte);
+    
+    ///Print decoded information
+    //
+    ///This function prints the decoded information after receiving a bitstream. It takes
+    /// no parameters and returns nothing.
     void get_decoded_byte();
 };
 
--- 0-Versie-Protocol/send_ir_message.hpp
+++ 0-Versie-Protocol/send_ir_message.hpp
@@ -3,19 +3,60 @@
 #ifndef SEND_IR_MESSAGE_HPP
 #define SEND_IR_MESSAGE_HPP
 
+///Class for sending IR message
+//
+///This class has been created to easily send an IR message. It uses the protocol specified
+/// in the document "V2THDE - Casus lasertag 2016-2017.pdf". The class uses hwlib created
+// by Wouter van Ooijen to make programming in c++ for Arduino and other microcontrollers
+/// more easy.
 class send_ir_message
 {
 private:
-    hwlib::target::d2_36kHz  & sender_pin;
+    hwlib::target::d2_36kHz  & sender_pin; ///pin the IR led is attached to
     int identifier;
     int weapon;
-    int sending_byte;
+    int sending_byte;///encoded message to send to the receiver
 public:
+    ///Default constructor
+    //
+    ///The default constructor of this class needs to receive a pin the IR led is attached to,
+    /// a player identifier and a weapon identifier. These are all used to create the commands
+    /// we are sending using the IR protocol. The default constructor also immediatly encodes
+    /// the values received as parameters into the encode function (see below) to create the
+    /// message that can be send.
     send_ir_message(hwlib::target::d2_36kHz  & sender_pin, int identifier, int weapon);
+    
+    ///Default destructor
+    //
+    ///The destructor frees up resources that have been used by this class so that other classes
+    /// can use them.
     ~send_ir_message();
+    
+    ///Send a one over the IR pin
+    //
+    ///This function sends a one using the IR LED in the way specified in the above mentioned
+    /// document. It takes no parameters and dus not return anything. It is here to make sending
+    /// a one in the rest of the class more easy.
     void send_one();
+    
+    ///Send a zero over the IR pin
+    //
+    ///This function sends a zero using the IR LED in the way specified in the above mentioned
+    /// document. It takes no parameters and dus not return anything. It just makes sending a
+    /// zero much more easy.
     void send_zero();
+    
+    ///Sending a message
+    //
+    ///This function sends the endcoded message wich was created in the default constructor.
+    ///It requires no parameters and returns nothing. It just sends the message twice. This
+    /// happens for acuracy.
     void send_byte();
+    
+    ///Encode the message
+    //
+    ///This functions creates the message we send later. It dus not need any parameters.
+    ///The return value is the final message that is send over the IR LED.
     int encoder();
 };
 
