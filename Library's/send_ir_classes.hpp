661921a6617313f409cf3e2b014852a5262884f1
timijntema <klimtim@hotmail.com>
2016-11-09 17:10:59 +0100

    sender finally done
    sender finally done

--- Library's/send_ir_classes.hpp
+++ Library's/send_ir_classes.hpp
@@ -1,4 +1,5 @@
 #include "hwlib.hpp"
+#include "entity_classes.hpp"
 #include "rtos.hpp"
 
 #ifndef SEND_IR_CLASSES_HPP
@@ -6,83 +7,94 @@
 
 class ir_sender{
 private:
-    hwlib::target::d2_36kHz IR_led_pin;
-public:
-    ir_sender(hwlib::pin_out & ir_pin):
-        IR_led_pin(ir_pin)
-    {}
-    
-    void set(bool val){
-        if(val){
-            set(1);
-            hwlib::wait_us(1600);
-            set(0);
-            hwlib::wait_us(800);
-        }
-        else{
-            set(1);
-            hwlib::wait_us(800);
-            set(0);
-            hwlib::wait_us(1600);
-        }
+    hwlib::target::d2_36kHz sender_pin;
+public:    
+    void send_one(){
+        sender_pin.set(1);
+        hwlib::wait_us(1600);
+        sender_pin.set(0);
+        hwlib::wait_us(800);
     }
     
-    ///Sending a message
-    //
-    ///This function sends the endcoded message wich was created in the default constructor.
-    ///It requires no parameters and returns nothing. It just sends the message twice. This
-    /// happens for acuracy.
+    void send_zero(){
+        sender_pin.set(1);
+        hwlib::wait_us(800);
+        sender_pin.set(0);
+        hwlib::wait_us(1600);
+    }
+    
+    void set_zero(){
+        sender_pin.set(0);
+    }
+    
     void send_message(char16_t compiled_message){
         for(int amount_messages = 0; amount_messages < 2; amount_messages++){
             for(int i = 15; i >= 0; i--){
                 if((compiled_message & (1 << i)) != 0){
-                    set(1);
-                }else{
-                    set(0);
+                    send_one();
+                }
+                else{
+                    send_zero();
                 }
             }
-            
+            sender_pin.set(0);
             hwlib::wait_ms(3);
         }
-    }
+        
+}
 };
 
-///Class for sending IR message
-//
-///This class has been created to easily send an IR message. It uses the protocol specified
-/// in the document "V2THDE - Casus lasertag 2016-2017.pdf". The class uses hwlib created
-// by Wouter van Ooijen to make programming in c++ for Arduino and other microcontrollers
-/// more easy.
-class ir_message_controller : public rtos::task<>{
+
+class send_controller : public rtos::task<>{
 private:
-    ir_sender ir_led;
-    my_player_information & player_information;
-    rtos::flag name_of_flag;
+    ir_sender ir_send;
+    my_player_information &message_to_send;
+    
     void main(){
         while(1){
-            wait(name_of_flag);
-            ir_led.send_message(player_information.get_compiled_bits());
+            send_full_message();
+            sleep(2000*rtos::ms);
         }
     }
 public:
-    ///Default constructor
-    //
-    ///The default constructor of this class needs to receive a pin the IR led is attached to,
-    /// a player identifier and a weapon identifier. These are all used to create the commands
-    /// we are sending using the IR protocol. The default constructor also immediatly encodes
-    /// the values received as parameters into the encode function (see below) to create the
-    /// message that can be send.
-    //send_ir_message(hwlib::target::d2_36kHz & IR_led_pin, byte player_id = 0, byte weapon_id = 0):
-    ir_message_controller(hwlib::pin_out & ir_pin, my_player_information & player_information):
-        ir_led( ir_pin ),
-        player_information(player_information),
-        name_of_flag(this, "name_of_flag")
-        
+    send_controller(my_player_information & player_information):
+    task("send_task"),
+    ir_send(),
+    message_to_send(player_information)
     {}
-    
-    void enable_flag(){
-        name_of_flag.set();
+
+    void send_full_message(){
+        char16_t message = message_to_send.get_compiled_bits();
+        ir_send.send_message(message);
     }
 };
+/*
+void send_one(auto sender_pin){
+        sender_pin.set(1);
+        hwlib::wait_us(1600);
+        sender_pin.set(0);
+        hwlib::wait_us(800);
+    }
+    
+    void send_zero(auto sender_pin){
+        sender_pin.set(1);
+        hwlib::wait_us(800);
+        sender_pin.set(0);
+        hwlib::wait_us(1600);
+    }
 
-#endif // SEND_IR_CLASSES_HPP
+int main(void){
+    //kill the watchdog
+    WDT->WDT_MR=WDT_MR_WDDIS;
+    hwlib::wait_ms(1000);
+    
+    hwlib::cout << "Starting program .. \n";
+    auto player = my_player_information();
+    player.set_compiled_bits(46774);
+    auto sender = send_controller(player);
+    
+    rtos::run();
+    
+}*/
+
+#endif //SEND_IR_CLASSES_HPP
